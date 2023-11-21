34e6f22d1e3d3e487fa598195804b022f49805d3
altares <altares@multitel.be>
2023-11-21 10:58:39 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+name='strats_pytorch',
+version='1.0.0',
+author='Cyprien Gille',
+author_email='cyprien.gille@umons.ac.be',
+packages=['models',
+],
+include_package_data=True,
+package_data={},
+scripts=[],
+url='https://github.com/CyprienGille/STraTS-PyTorch',
+license='LICENSE',
+description='',
+long_description=open('README.md').read(),
+long_description_content_type='text/markdown',
+install_requires=[
+
+],
+)
