2c41781a3c80fd830859919fd977a4a99b8bd70c
CyprienGille <>
2024-04-08 14:58:49 +0200

    Add STraTS_Dense and activation function setting

--- strats_pytorch/models/strats.py
+++ strats_pytorch/models/strats.py
@@ -21,7 +21,9 @@ class STraTS(nn.Module):
         n_heads: int = 4,
         dim_ff: int = None,
         dropout: float = 0.0,
+        activation: str = "relu",
         forecasting: bool = False,
+        regression: bool = False,
         n_classes: int = 2,
     ) -> None:
         super().__init__()
@@ -35,11 +37,17 @@ class STraTS(nn.Module):
         self.cve_values = CVE(hid_units=int(sqrt(dim_embed)), output_dim=dim_embed)
         self.cve_times = CVE(hid_units=int(sqrt(dim_embed)), output_dim=dim_embed)
         self.trans = TransformerEncoderWrapper(
-            dim_embed, n_layers, n_heads, dim_ff, dropout
+            dim_embed,
+            n_layers,
+            n_heads,
+            dim_ff,
+            dropout,
+            activation,
         )
         self.attn = Attention(dim_embed, hid_dim=2 * dim_embed)
 
         self.forecasting = forecasting
+        self.regression = regression
 
         self.ouput_forecasting = nn.Linear(
             in_features=2 * dim_embed, out_features=n_var_embs
@@ -47,6 +55,7 @@ class STraTS(nn.Module):
         self.ouput_prediction = nn.Linear(
             in_features=2 * dim_embed, out_features=n_classes
         )
+        self.output_regression = nn.Linear(in_features=2 * dim_embed, out_features=1)
 
     def forward(
         self,
@@ -56,22 +65,95 @@ class STraTS(nn.Module):
         variables: torch.Tensor,
         mask: Optional[torch.Tensor] = None,
     ):
-        # This just follows figure 3 of the STraTS paper
-        # Note: the mask is for unobserved variables
-
         demo_emb = self.lin_2_demog(self.tanh_demog(self.lin_1_demog(demog)))
         var_emb = self.var_embedding(variables.long())
         values_emb = self.cve_values(values)
         times_emb = self.cve_times(times)
         combined_emb = (
             var_emb + values_emb + times_emb
-        )  # TODO: Better way of combining the embeddings?
+        )  # TODO better embeddings combination
         contextual_emb = self.trans(combined_emb, src_key_padding_mask=mask)
         attn_weights = self.attn(contextual_emb, mask=mask)
         fused_emb = torch.sum(contextual_emb * attn_weights, dim=-2)
         concat = torch.concat((fused_emb, demo_emb), dim=1)
         if self.forecasting:
             return self.ouput_forecasting(concat)
+        elif self.regression:
+            return self.output_regression(concat).squeeze()
+        return self.ouput_prediction(concat).squeeze()
+
+
+class STraTS_Dense(nn.Module):
+    """A variant of STraTS where the val/time/var embeddings are combined through a FCN"""
+
+    def __init__(
+        self,
+        n_var_embs: int,
+        dim_demog: int = 2,
+        dim_embed: int = 52,
+        n_layers: int = 2,
+        n_heads: int = 4,
+        dim_ff: int = None,
+        dropout: float = 0.0,
+        activation: str = "relu",
+        forecasting: bool = False,
+        regression: bool = False,
+        n_classes: int = 2,
+    ) -> None:
+        super().__init__()
+
+        self.lin_1_demog = nn.Linear(in_features=dim_demog, out_features=2 * dim_embed)
+        self.lin_2_demog = nn.Linear(in_features=2 * dim_embed, out_features=dim_embed)
+        self.tanh_demog = nn.Tanh()
+        self.var_embedding = nn.Embedding(
+            num_embeddings=n_var_embs, embedding_dim=dim_embed
+        )
+        self.cve_values = CVE(hid_units=int(sqrt(dim_embed)), output_dim=dim_embed)
+        self.cve_times = CVE(hid_units=int(sqrt(dim_embed)), output_dim=dim_embed)
+        self.dense_agg = nn.Linear(in_features=3 * dim_embed, out_features=dim_embed)
+        self.trans = TransformerEncoderWrapper(
+            dim_embed,
+            n_layers,
+            n_heads,
+            dim_ff,
+            dropout,
+            activation,
+        )
+        self.attn = Attention(dim_embed, hid_dim=2 * dim_embed)
+
+        self.forecasting = forecasting
+        self.regression = regression
+
+        self.ouput_forecasting = nn.Linear(
+            in_features=2 * dim_embed, out_features=n_var_embs
+        )
+        self.ouput_prediction = nn.Linear(
+            in_features=2 * dim_embed, out_features=n_classes
+        )
+        self.output_regression = nn.Linear(in_features=2 * dim_embed, out_features=1)
+
+    def forward(
+        self,
+        demog: torch.Tensor,
+        values: torch.Tensor,
+        times: torch.Tensor,
+        variables: torch.Tensor,
+        mask: Optional[torch.Tensor] = None,
+    ):
+        demo_emb = self.lin_2_demog(self.tanh_demog(self.lin_1_demog(demog)))
+        var_emb = self.var_embedding(variables.long())
+        values_emb = self.cve_values(values)
+        times_emb = self.cve_times(times)
+        combined_emb = torch.concat((var_emb, values_emb, times_emb), dim=2)
+        aggregated_emb = self.dense_agg(combined_emb)
+        contextual_emb = self.trans(aggregated_emb, src_key_padding_mask=mask)
+        attn_weights = self.attn(contextual_emb, mask=mask)
+        fused_emb = torch.sum(contextual_emb * attn_weights, dim=-2)
+        concat = torch.concat((fused_emb, demo_emb), dim=1)
+        if self.forecasting:
+            return self.ouput_forecasting(concat)
+        elif self.regression:
+            return self.output_regression(concat).squeeze()
         return self.ouput_prediction(concat).squeeze()
 
 
