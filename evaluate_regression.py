1dfa5fb6d05777b9b16a66ada60f7e0bbf5d1924
CyprienGille <>
2024-04-09 16:22:45 +0200

    Documentation

--- evaluate_regression.py
+++ evaluate_regression.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
@@ -22,7 +24,39 @@ def get_model_and_dl(
     n_layers=5,
     dim_embed=104,
     n_heads=4,
-):
+) -> Tuple[STraTS, DataLoader]:
+    """Get a trained model and the corresponding testing dataloader
+
+    Parameters
+    ----------
+    model_dir : str
+        The path to the model dir, for example 'exp_creat_reg/exp_14/'
+    data_path : str
+        The path to the preprocessed data
+    back_interval : int, optional
+        The observation window width in minutes, by default 2880
+    n_vars : int, optional
+        The number of features in the data, by default 29
+    var_id : int, optional
+        The id of the target variable, by default 0
+    normalize_times : bool, optional
+        Whether to normalize times, by default True
+    normalize_vars : bool, optional
+        Whether to normalize variables, by default True
+    verbose : bool, optional
+        Whether to display progress bars, by default False
+    n_layers : int, optional
+        The number of transformer layers in the model, by default 5
+    dim_embed : int, optional
+        The embedding dimension of the model, by default 104
+    n_heads : int, optional
+        The number of heads in the model, by default 4
+
+    Returns
+    -------
+    Tuple[STraTS, DataLoader]
+        The model with its loaded weights, and the test DataLoader
+    """
     ds = MIMIC_Reg(data_path, var_id=var_id, crop_back_interval=back_interval)
     ds.restrict_to_indexes(np.load(model_dir + "test_idx.npy"))
     ds.normalize(normalize_vars, normalize_times, verbose=verbose)
